8b9ca6ef019185765edd81971a33f346652bd10e
Peter Schueller <peter@peterschueller.com>
2021-05-31 14:34:36 +0200

    rethrowing in second code duplicateion

--- TOSCAGeneratorClient/src/main/java/org/acumos/designstudio/toscagenerator/ToscaGeneratorClient.java
+++ TOSCAGeneratorClient/src/main/java/org/acumos/designstudio/toscagenerator/ToscaGeneratorClient.java
@@ -271,6 +271,7 @@ public class ToscaGeneratorClient {
 		} catch (AcumosException e) {
 			logger.error(" Exception in  TOSCA Model Generator Client ", e);
 			result = String.format(error, e.getErrorCode(), e.getErrorDesc());
+			throw e;
 		} catch (Exception e) {
 			logger.error(" Exception in  TOSCA Model Generator Client ", e);
 			result = String.format(error, Properties.getTOSCAFileGenerationErrorCode(),
