22df6aff425824490d9af2e5ddc4346d69861b36
Peter Schueller <peter@peterschueller.com>
2021-05-31 14:20:44 +0200

    still not failing - logging

--- onboarding-common/src/main/java/org/acumos/onboarding/services/impl/OnboardingController.java
+++ onboarding-common/src/main/java/org/acumos/onboarding/services/impl/OnboardingController.java
@@ -957,6 +957,7 @@ public class OnboardingController extends CommonOnboarding implements DockerServ
 					if(protobuf != null && !protobuf.isEmpty()) {
 						generateTOSCA(localProtobufFile, null, mData, onboardingStatus);
 					}
+					logger.error("after generateTOSCA");
 
 					// Notify TOSCA generation successful
 					if (onboardingStatus != null) {
@@ -1024,6 +1025,7 @@ public class OnboardingController extends CommonOnboarding implements DockerServ
 									+ mlpSolution.getSolutionId() + ", Status Code: " + res.getStatusCode());
 					return res;
 				} finally {
+					logger.error("FINALLY in finally");
 
 					try {
 						UtilityFunction.deleteDirectory(outputFolder);
@@ -1102,7 +1104,10 @@ public class OnboardingController extends CommonOnboarding implements DockerServ
 				}
 			}
 
+			logger.error("leaving without exception");
+
 		} catch (AcumosServiceException e) {
+			logger.error("leaving with acumos service exception");
 
 			HttpStatus httpCode = HttpStatus.INTERNAL_SERVER_ERROR;
 			MDC.put(OnboardingLogConstants.MDCs.RESPONSE_STATUS_CODE, OnboardingLogConstants.ResponseStatus.ERROR.name());
@@ -1115,6 +1120,8 @@ public class OnboardingController extends CommonOnboarding implements DockerServ
 			return new ResponseEntity<ServiceResponse>(
 					ServiceResponse.errorResponse(e.getErrorCode(), e.getMessage(), modelName), httpCode);
 		} catch (HttpClientErrorException e) {
+			logger.error("leaving with httpclient error exception");
+
 			// Handling #401 and 400(BAD_REQUEST) is added as CDS throws 400 if apitoken is
 			// invalid.
 			MDC.put(OnboardingLogConstants.MDCs.RESPONSE_STATUS_CODE, OnboardingLogConstants.ResponseStatus.ERROR.name());
@@ -1134,6 +1141,8 @@ public class OnboardingController extends CommonOnboarding implements DockerServ
 						e.getStatusCode());
 			}
 		} catch (Exception e) {
+			logger.error("leaving with exception");
+
 			MDC.put(OnboardingLogConstants.MDCs.RESPONSE_STATUS_CODE, OnboardingLogConstants.ResponseStatus.ERROR.name());
 			MDC.put(OnboardingLogConstants.MDCs.RESPONSE_DESCRIPTION, HttpStatus.INTERNAL_SERVER_ERROR.toString());
 			MDC.put(OnboardingLogConstants.MDCs.RESPONSE_CODE, HttpStatus.INTERNAL_SERVER_ERROR.toString());
