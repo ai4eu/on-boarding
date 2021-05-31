55d2107af27e6a112af052bca99ce38dd73eee07
Peter Schueller <peter@peterschueller.com>
2021-05-31 10:36:49 +0200

    AI4EUEXP-125 deactivating protobuf versioning because then a lot of code is removable

--- onboarding-common/src/main/java/org/acumos/onboarding/services/impl/CommonOnboarding.java
+++ onboarding-common/src/main/java/org/acumos/onboarding/services/impl/CommonOnboarding.java
@@ -919,8 +919,9 @@ public class CommonOnboarding {
 
 		//Check if the lastProtobuf is not null
 		if (lastProtobufString != null && !lastProtobufString.isEmpty()) {
-			protoBuf1 = ProtobufUtil.parseProtobuf(lastProtobufString);
-			protoBuf2 = ProtobufUtil.parseProtobuf(currentProtobufString);
+			// protoBuf1 = ProtobufUtil.parseProtobuf(lastProtobufString);
+			// protoBuf2 = ProtobufUtil.parseProtobuf(currentProtobufString);
+			throw new RuntimeError("there was code here that we commented out: compare protobuf versions");
 		}
 		String verA = countMajor;
 		String verB = countMinor;
